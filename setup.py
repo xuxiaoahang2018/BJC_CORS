4ddadf14e74bba20533afc944d99d5f08fe9d051
Evan <36877025+xuxiaoahang2018@users.noreply.github.com>
2020-12-29 12:55:39 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name = "bjc-cors",
+    version = "0.0.1",
+    url = "None",
+    description = "Dynamic cross domain project implemented by Python flash",
+    author = "evan",
+    author_email = "824452323@qq.com",
+    requires=["flask","pymysql","jwt"],
+)
